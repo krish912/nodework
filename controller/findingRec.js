1f75ce39f5d13d3d677da6491ff02eefb3e4091e
krish912 <163541993+krish912@users.noreply.github.com>
2025-09-08 19:23:45 +0530

    Update findingRec.js

--- controller/findingRec.js
+++ controller/findingRec.js
@@ -1,22 +1,31 @@
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017";
-const client = new MongoClient(url);
-const dbName = 'local';
+//i paused the database coonectivity for live 
+
+// var MongoClient = require('mongodb').MongoClient;
+// var url = "mongodb://localhost:27017";
+// const client = new MongoClient(url);
+// const dbName = 'local';
 const Stu = require('../data/studentdata.json');
 const findingRec = async (req,res)=>{
-    await client.connect();
-    console.log('Connected successfully to server');
-    const db = client.db(dbName);
-    console.log("db Connected successfully to server");
-    const collection = db.collection('studentData');
-    const findResult = await collection.find({}).toArray();
-    console.log('Found documents =>', findResult);
-      res.send(
-        {
-            status:200,
-            listofuser: findResult
-        }
-    )
+    //await client.connect();
+    //console.log('Connected successfully to server');
+    //const db = client.db(dbName);
+    // console.log("db Connected successfully to server");
+    // const collection = db.collection('studentData');
+    // const findResult = await collection.find({}).toArray();
+    // console.log('Found documents =>', findResult);
+      try {
+        // Send the student data directly from JSON
+        res.send({
+            status: 200,
+            listofuser: Stu
+        });
+    } catch (e) {
+        // Handle errors gracefully
+        res.status(500).send({
+            status: 500,
+            message: e.message
+        });
+}
 }
 module.exports = {
     findingRec
